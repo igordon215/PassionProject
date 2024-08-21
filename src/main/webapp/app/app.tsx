ac466671e405b26b80c6049dbf042efb209bfb46
igordon215 <igordon215@gmail.com>
2024-08-21 08:57:57 -0400

    Revert "Update routing and layout in app.tsx"
    This reverts commit dc1612cc3425873be972ceedc9e35ff07f740d91.

--- src/main/webapp/app/app.tsx
+++ src/main/webapp/app/app.tsx
@@ -3,16 +3,20 @@ import './app.scss';
 import 'app/config/dayjs';
 
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import { useAppDispatch, useAppSelector } from 'app/config/store';
 import { getSession } from 'app/shared/reducers/authentication';
 import { getProfile } from 'app/shared/reducers/application-profile';
 import ErrorBoundary from 'app/shared/error/error-boundary';
-import AppRoutes from 'app/routes';
-import Header from 'app/shared/layout/header/header';
-import Footer from 'app/shared/layout/footer/footer';
+import { AUTHORITIES } from 'app/config/constants';
+import { hasAnyAuthority } from 'app/shared/auth/private-route';
+
+import Header from 'app/modules/forum/Header';
+import ForumList from 'app/modules/forum/ForumList';
+import ForumPost from 'app/modules/forum/ForumPost';
+import CreatePost from 'app/modules/forum/CreatePost';
 
 export const App = () => {
   const dispatch = useAppDispatch();
@@ -22,6 +26,9 @@ export const App = () => {
     dispatch(getProfile());
   }, []);
 
+  const isAuthenticated = useAppSelector(state => state.authentication.isAuthenticated);
+  const isAdmin = useAppSelector(state => hasAnyAuthority(state.authentication.account.authorities, [AUTHORITIES.ADMIN]));
+
   return (
     <Router>
       <div className="app-container">
@@ -29,12 +36,15 @@ export const App = () => {
         <ErrorBoundary>
           <Header />
         </ErrorBoundary>
-        <div className="container-fluid view-container" id="app-view-container">
+        <div className="container mt-3">
           <ErrorBoundary>
-            <AppRoutes />
+            <Routes>
+              <Route path="/" element={<ForumList />} />
+              <Route path="/forum/:id" element={<ForumPost />} />
+              <Route path="/forum/new" element={isAuthenticated ? <CreatePost /> : <ForumList />} />
+            </Routes>
           </ErrorBoundary>
         </div>
-        <Footer />
       </div>
     </Router>
   );
